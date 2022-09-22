5eaf69e884dc28f0dd6a5c90369847e17b037f33
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2022-09-22 17:41:42 -0500

    Initial commit

---
+++ warm-up-exercise1.js
@@ -0,0 +1,8 @@
+//***Evaluate the expression of a and b depending on what operator is (..see defined function below). */
+
+function calculate(a, b, operator) {
+    
+  }
+  
+  calculate(1,1, "+"); // 2
+  calculate(2, 2, '-'); // 0
