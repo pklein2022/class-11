4cfc2ffa414b08277b339b067210bcd786118e2d
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2022-09-24 09:57:43 -0500

    completed exercises

--- warm-up-exercise1.js
+++ warm-up-exercise1.js
@@ -1,7 +1,13 @@
 //***Evaluate the expression of a and b depending on what operator is (..see defined function below). */
 
 function calculate(a, b, operator) {
-    
+    let evaluation = 0
+    if (operator === "+"){
+        evaluation = a + b;
+        else (operator === "-"){
+            evaluation = a - b;
+        }
+    }
   }
   
   calculate(1,1, "+"); // 2
---
+++ warm-up-exercise2.js
@@ -0,0 +1,13 @@
+//Given the array - Print each object's id, status of completion and action onto the page.
+  let list = [
+    {
+      id: 1, 
+      completed: true,
+      action: "Study"
+    },
+    {
+      id: 2,
+      completed: false, 
+      action: "Dishes"
+    }
+  ]
