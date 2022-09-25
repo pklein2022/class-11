992984d118489b9d5d3092a73fb316ece7cfbb15
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2022-09-25 15:50:43 -0500

    exercise-7 bonus

--- exercise-7/angular-basics-exercise/src/app/article/article.component.ts
+++ exercise-7/angular-basics-exercise/src/app/article/article.component.ts
@@ -10,4 +10,23 @@ export class ArticleComponent {
   title: string = "Whatever you want";
   content: string = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis quisquam, dolorem qui optio temporibus quo officia harum suscipit! Voluptates, odio mollitia! Aspernatur autem quisquam et cumque officiis iure eos nam!";
   isTechRelated: boolean = true;
+
+  onToggleButton() {
+    // if (this.isTechRelated === true) {
+    //   this.isTechRelated = false;
+    // } else {
+    //   this.isTechRelated = true;
+    // }
+
+    // if (this.isTechRelated) {
+    //   this.isTechRelated = false;
+    // } else {
+    //   this.isTechRelated = true;
+    // }
+
+    // this.isTechRelated = this.isTechRelated ? false : true;
+
+    this.isTechRelated = !this.isTechRelated;
+  }
 }
+
